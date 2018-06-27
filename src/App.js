771bd2226d4865256b94059c221a04aeb8a18e5b
Usman Bashir <me@usmanbashir.com>
2018-06-27 22:02:47 +0300

    The Starting Line

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Welcome to the Workshop</h1>
+    );
+  }
+}
+
+export default App;
